d5d8f3a3da1e7c257258f65289f5cdc4799208d2
ldiez <ldiez@tlmat.unican.es>
2022-03-28 12:10:29 +0200

    ++

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int MainFunctionality (int i) {
+  return i;
+}
+
 int main()
 {
+  int ret = 0;
+  for (int i = 0; i < 10; ++i){
+    // Main functionality check
+    if (MainFunctionality(i) == i){
+      printf("SUCCESS: main functionality %d"\n, i);
+    } else {
+      printf("FAILED: main functionality %d\n", i);
+      ret = -1;
+    }
 
-  printf("Hello world\n");
-  printf("changes in main\n");
-  printf("Changes in second branchh\n");
+  }
+
+  return ret;
 }
